0cfca11c61c2ff78718a69e2475536a411b22ca3
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2021-05-20 10:45:11 +0200

    Create pm2.sh

---
+++ pm2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt update && apt upgrade -y
+apt install curl -y
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt install nodejs -y
+apt install build-essential
+rm ./nodesource_setup.sh
+echo "NodeJS à bien été installé"
+npm i pm2 -g
